c2ac1ce0d16d203b78fe064ca48ff2c651e8cfd2
18571158353 <2730747608@qq.com>
2021-04-19 22:27:21 +0800

    登录登出版块完成

--- components/my-settle/my-settle.vue
+++ components/my-settle/my-settle.vue
@@ -11,7 +11,7 @@
     </view>
 
     <!-- 结算按钮 -->
-    <view class="btn-settle">结算({{checkedCount}})</view>
+    <view class="btn-settle" @click="settlement">结算({{checkedCount}})</view>
   </view>
 </template>
 
@@ -19,17 +19,25 @@
   // 1. 按需导入 mapMutations 辅助函数
   import {
     mapGetters,
-    mapMutations
+    mapMutations,
+    mapState
   } from 'vuex'
   export default {
     name: "my-settle",
     data() {
       return {
-
+        // 倒计时的秒数
+        seconds: 3,
+        // 定时器的 Id
+        timer: null
       };
     },
     computed: {
       ...mapGetters('m_cart', ['total', 'checkedCount', 'checkedGoodsAmount']),
+      // addstr 是详细的收货地址
+      ...mapGetters('m_user', ['addstr']),
+      // token 是用户登录成功之后的 token 字符串
+      ...mapState('m_user', ['token']),
       // 2. 是否全选
       isFullCheck() {
         return this.total === this.checkedCount
@@ -38,10 +46,79 @@
     methods: {
       // 2. 使用 mapMutations 辅助函数，把 m_cart 模块提供的 updateAllGoodsState 方法映射到当前组件中使用
       ...mapMutations('m_cart', ['updateAllGoodsState']),
+      // 把 m_user 模块中的 updateRedirectInfo 方法映射到当前页面中使用
+      ...mapMutations('m_user', ['updateRedirectInfo']),
       changeAllState() {
         // 修改购物车中所有商品的选中状态
         // !this.isFullCheck 表示：当前全选按钮的状态取反之后，就是最新的勾选状态
         this.updateAllGoodsState(!this.isFullCheck)
+      },
+      // 点击了结算按钮
+      settlement() {
+        // 1. 先判断是否勾选了要结算的商品
+        if (!this.checkedCount) return uni.$showMsg('请选择要结算的商品！')
+
+        // 2. 再判断用户是否选择了收货地址
+        if (!this.addstr) return uni.$showMsg('请选择收货地址！')
+
+        // 3. 最后判断用户是否登录了，如果没有登录，则调用 delayNavigate() 进行倒计时的导航跳转
+        // if (!this.token) return uni.$showMsg('请先登录！')
+        if (!this.token) return this.delayNavigate()
+      },
+      // 延迟导航到 my 页面
+      delayNavigate() {
+        // 把 data 中的秒数重置成 3 秒
+        this.seconds = 3
+        // 1. 展示提示消息，此时 seconds 的值等于 3
+        this.showTips(this.seconds)
+
+        // 2. 创建定时器，每隔 1 秒执行一次
+        // 1. 将定时器的 Id 存储到 timer 中
+        this.timer = setInterval(() => {
+          // 2.1 先让秒数自减 1
+          this.seconds--
+
+          // 2. 判断秒数是否 <= 0
+          if (this.seconds <= 0) {
+            // 2.1 清除定时器
+            clearInterval(this.timer)
+
+            // 2.2 跳转到 my 页面
+            uni.switchTab({
+              url: '/pages/my/my',
+              // 页面跳转成功之后的回调函数
+              success: () => {
+                // 调用 vuex 的 updateRedirectInfo 方法，把跳转信息存储到 Store 中
+                this.updateRedirectInfo({
+                  // 跳转的方式
+                  openType: 'switchTab',
+                  // 从哪个页面跳转过去的
+                  from: '/pages/cart/cart'
+                })
+              }
+            })
+
+            // 2.3 终止后续代码的运行（当秒数为 0 时，不再展示 toast 提示消息）
+            return
+          }
+
+          // 2.2 再根据最新的秒数，进行消息提示
+          this.showTips(this.seconds)
+        }, 1000)
+      },
+      // 展示倒计时的提示消息
+      showTips(n) {
+        // 调用 uni.showToast() 方法，展示提示消息
+        uni.showToast({
+          // 不展示任何图标
+          icon: 'none',
+          // 提示的消息
+          title: '请登录后再结算！' + n + ' 秒后自动跳转到登录页',
+          // 为页面添加透明遮罩，防止点击穿透
+          mask: true,
+          // 1.5 秒后自动消失
+          duration: 1500
+        })
       }
     },
   }
--- pages/my/my.vue
+++ pages/my/my.vue
@@ -1,10 +1,16 @@
 <template>
-  <view>
-    My
+  <view class="my-container">
+    <!-- 用户未登录时，显示登录组件 -->
+    <my-login v-if="!token"></my-login>
+    <!-- 用户登录后，显示用户信息组件 -->
+    <my-userinfo v-else></my-userinfo>
   </view>
 </template>
 
 <script>
+  import {
+    mapState
+  } from 'vuex'
   // 导入自己封装的 mixin 模块
   import badgeMix from '@/mixins/tabbar-badge.js'
   export default {
@@ -13,10 +19,15 @@
       return {
 
       };
+    },
+    computed: {
+      ...mapState('m_user', ['token'])
     }
   }
 </script>
 
 <style lang="scss">
-
+  .my-container {
+    height: 100%;
+  }
 </style>
